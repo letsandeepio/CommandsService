87412b60e78e95cd482b82d91464951f40742462
Sandeep <sandeepchopra7@gmail.com>
2023-10-19 18:55:44 -0400

    add Command Repository

---
+++ Data/ICommandRepos.cs
@@ -0,0 +1,20 @@
+using CommandService.Models;
+
+namespace CommandService.Data
+{
+  public interface ICommandRepo
+  {
+    bool SaveChanges();
+
+
+    //methods for platforms 
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlatformExists(int platformId);
+
+    //methos for commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+  }
+}
