5756ce133a0d94fb14cfa63bdd6e01ea6822a592
Sandeep <sandeepchopra7@gmail.com>
2023-10-20 14:44:05 -0400

    add get endpoint

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using CommandService.Data;
+using CommandService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandService.Controllers
@@ -16,6 +17,14 @@ namespace CommandService.Controllers
     {
       _repository = repository;
       _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+      Console.WriteLine("--> Getting Platforms from CommandsService");
+      var platformItems = _repository.GetAllPlatforms();
+      return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
     }
 
     [HttpPost]
